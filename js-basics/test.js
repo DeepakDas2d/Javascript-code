5e0890b01d734c2cd725ea12c3f507f6198b1d45
deepakdas2d <contacttodeepakdas@gmail.com>
2024-11-22 17:13:46 +0530

    Hoisting Example

---
+++ js-basics/test.js
@@ -0,0 +1,11 @@
+// console.log(x);
+// var x = 5;
+// console.log(x);
+
+// console.log(x); // ReferenceError: Cannot access 'x' before initialization
+// let x = 5;
+// console.log(x);
+
+console.log(x); // ReferenceError: Cannot access 'x' before initialization
+const x = 5;
+console.log(x);
